e970a40d01d6191f84576ad2dae362394a5b4245
Tlesis <jackwilliamson.1215@gmail.com>
2026-02-20 13:53:32 -0600

    should be task 1.1

--- src/eecs_388_pwm.cpp
+++ src/eecs_388_pwm.cpp
@@ -31,34 +31,59 @@
 //         340Ω  |    340Ω  340Ω
 //          |    |     |     MCU
 //         MCU   |    MCU    Pin
-//         Pin  GND   Pin 
+//         Pin  GND   Pin
 //              Pin
+#ifdef BONUS_TASK
+static void led() {}
+#endif // BONUS_TASK
 
-//static void led()
-//{
-//
-//return;
-//}
+// bounded formula to get the pulse length from the angle
+//  this is an approximation
+static double getPulseLength(int angle) {
+    // formula to get the pulse length from the angle
+    double p = ((double)angle / 0.18) + 1000;
+    // bound p; SERVO_PULSE_MIN <= p <= SERVO_PULSE_MAX (i.e. [544, 2400])
+    return max(SERVO_PULSE_MIN, min(p, SERVO_PULSE_MAX));
+}
 
 /******************************************************************************
  *   Function: servo() - Servo Motor Pulse
- *      Pre condition: 
+ *      Pre condition:
  *          Assumes GPIO pin used has been configured for output
- *      Post condition: 
+ *      Post condition:
  *          Calculates a duty cycle in microseconds for the amount of on time
  *          needed to achieve the angle passed in
  *******************************************************************************/
-static void servo (int gpio, int angle) {
+static void servo(int gpio, int angle) {
     // YOUR CODE HERE
     // Basically, you need to take the input angle "angle" and generate the corresponding pwm signal
     // To generate the pwm signals, use gpio_write(), delay_us(), and delay_ms().
+
+    double pulseLen_us = getPulseLength(angle);
+
+    //    ╭ 1-2ms
+    //   ┎----┓
+    //   |    |
+    //   |    |______________
+    //   ╚----- 20 ms-------╝
+    // turn on the gpio line for the given time
+    gpio_write(gpio, ON);
+    delay_us(pulseLen_us);
+    // turn off for the remaining time
+    gpio_write(gpio, OFF);
+    delay_us(SERVO_PERIOD - pulseLen_us);
+
+    // NOTE: technically there will be some amount of time between the end of
+    // this function call and the time it gets called again but it should be
+    // small enough to not worry about it (hopefully, I haven't checked thats
+    // just a gut feeling...)
 }
 
 /******************************************************************************
  *   Function: setup() - Initializes the Arduino System
- *      Pre condition: 
+ *      Pre condition:
  *          Hardware must be properly connected (sensors, LEDS, etc.)
- *      Post condition: 
+ *      Post condition:
  *          Runs initialization calls one time on power up
  *          Input/Output (IO) pins are configured
  *******************************************************************************/
@@ -66,9 +91,11 @@ void setup() {
     gpio_mode(GPIO_6, GPIO_OUTPUT);
 
     // Bonus Task - Uncomment and fill out this function if you attempt the bonus task
-    //gpio_mode(RED_LED, GPIO_OUTPUT);
-    //gpio_mode(BLUE_LED, GPIO_OUTPUT);
-    //gpio_mode(GREEN_LED, GPIO_OUTPUT);
+#ifdef BONUS_TASK
+    gpio_mode(RED_LED, GPIO_OUTPUT);
+    gpio_mode(BLUE_LED, GPIO_OUTPUT);
+    gpio_mode(GREEN_LED, GPIO_OUTPUT);
+#endif // BONUS_TASK
 }
 
 /******************************************************************************
@@ -83,6 +110,8 @@ void loop() {
     int gpio = GPIO_6;
 
     while (true) {
+
+#ifndef BONUS_TASK
         /**
          * Sweeps through angles 0 -> 180 by 30 to test the 'servo' function 
          *
@@ -97,9 +126,12 @@ void loop() {
                 servo(gpio, angle);
             }
         }
+#endif // !BONUS_TASK
 
+#ifdef BONUS_TASK
         // Bonus Task - Uncomment and fill out this function if you attempt the bonus task
         //            - Comment out the for loops and servo() function if you attempt the bonus task
         //led();
+#endif // BONUS_TASK
         }
 }
